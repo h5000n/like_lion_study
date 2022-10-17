2768c95f63d8219d929045eb6cad82975d64e7a9
hyeon <einsight1@naver.com>
2022-10-17 09:11:41 +0900

    star print

---
+++ class/src/java/week5/day1/StarPrint.java
@@ -0,0 +1,10 @@
+package week5.day1;
+
+public class StarPrint {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
