80b2421c039097efeeb45b24fb91a14c759deee3
hyeon <einsight1@naver.com>
2022-10-17 09:34:36 +0900

    star print by input

--- class/src/java/week5/day1/StarPrint.java
+++ class/src/java/week5/day1/StarPrint.java
@@ -3,14 +3,23 @@ package week5.day1;
 import java.util.Scanner;
 
 public class StarPrint {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+    String oneChar;
+    public StarPrint(String oneChar){
+        this.oneChar = oneChar;
+    }
+    void print(int num){
         for(int i = 0;i < num; i++){
             for(int j = 0; j <= i; j++){
-                System.out.printf("*");
+                System.out.printf(this.oneChar);
             }
             System.out.println();
         }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        StarPrint pt = new StarPrint("#");
+        pt.print(num);
     }
 }
