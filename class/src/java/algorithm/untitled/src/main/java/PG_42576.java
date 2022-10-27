8b8de310e8b8e3bb07005dcc53298f780a394c3a
hyeon <einsight1@naver.com>
2022-10-27 09:18:56 +0900

    PG_42576 not solved

---
+++ class/src/java/algorithm/untitled/src/main/java/PG_42576.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        int size;
+        int[] table= new int[size];
+        String answer = "";
+        return answer;
+    }
+    public Solution(int size, int[] table) {
+        this.size = size;
+        this.table = new int[size];
+    }
+}
