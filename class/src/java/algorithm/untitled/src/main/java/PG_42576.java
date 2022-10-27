e19176500a90591a709ab4248483c3c8eed48a63
hyeon <einsight1@naver.com>
2022-10-27 09:30:12 +0900

    PG_42576 not solved

--- class/src/java/algorithm/untitled/src/main/java/PG_42576.java
+++ class/src/java/algorithm/untitled/src/main/java/PG_42576.java
@@ -1,12 +1,21 @@
-class Solution {
-    public String solution(String[] participant, String[] completion) {
-        int size;
-        int[] table= new int[size];
-        String answer = "";
-        return answer;
+class MyHash{
+    private int size = 10000;
+    private int table[] = new int[size];
+
+    public MyHash() {
     }
-    public Solution(int size, int[] table) {
+
+    public MyHash(int size, int[] table) {
         this.size = size;
         this.table = new int[size];
+    }
+    public int hash(String key){
+        int asciiSum = 0;
+        for (int i = 0; i < key.length; i++){
+            char c = key.charAt(i);
+            int ascii = c;
+            asciiSum += ascii;
+        }
+        return asciiSum % 90;
     }
 }//
